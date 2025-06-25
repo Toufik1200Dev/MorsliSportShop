820136db617dff49f54e5af5fdac1fbbd9a7e6d7
rahmani toufik <rahmanitoufik1200@gmail.com>
2025-06-25 03:27:49 +0100

    fix: update CSP to allow Cloudinary images and media

--- Backend/config/middlewares.js
+++ Backend/config/middlewares.js
@@ -1,8 +1,41 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://morsli-sport-shop.web.app', // your Firebase Hosting frontend
+        'http://localhost:5173',             // for local dev
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
